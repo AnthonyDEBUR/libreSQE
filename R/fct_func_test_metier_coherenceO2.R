bda7adfe41cc52ca3ab4c97138859093ce5e31c7
AnthonyDEBUR <a.deburghrave@gmail.com>
2023-06-13 11:42:13 +0200

    DEV : assouplissement des tests de cohérence O2 pour limiter le nb de résultat incertains ou incorrects

--- R/fct_func_test_metier_coherenceO2.R
+++ R/fct_func_test_metier_coherenceO2.R
@@ -56,9 +56,9 @@ func_test_metier_coherenceO2<-function(O2=NULL, satO2=NULL, temp=NULL, export="c
   if(export=="code")
  { delta<-abs(satO2-satO2theorique)
 
-  out <- ifelse(delta < 2, "1",
-                ifelse(delta < 5, "2",
-                       ifelse(delta >= 5, "3", "0")))}
+  out <- ifelse(delta < 3, "1",
+                ifelse(delta < 6, "2",
+                       ifelse(delta >= 6, "3", "0")))}
   if(export=="value"){out<-satO2theorique}
 
   return(out)
